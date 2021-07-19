dbc63f9c9fbee1c00035e6ed020a02d6c5b481c3
luozuxiong <625846782@qq.com>
2021-07-20 07:01:21 +0800

    write

--- src/core/actions.js
+++ src/core/actions.js
@@ -49,24 +49,75 @@ class Work {
         }
         const folder = pathLib.resolve(path);
         const files = readSync(folder)
-
         this.sources = files.map(file => {
             return {
+                resolvePosition: file,
                 file: pathLib.resolve(folder, file)
             }
         });
 
-        console.log(this.sources)
 
     }
 
     parseContent() {
-        const fileParsePromise = this.sources.map(source => fs.readJson(source.file));
-        Promise.all(fileParsePromise).then(records => {
-            console.log(records)
-        })
+        const fileParsePromise = this.sources.map(source => new Promise((resolve, reject) => {
+            fs.readJson(source.file, (err, content) => {
+                if (err) {
+                    logger.error(`read json file error:${err}`);
+                    return reject({
+                        file: source.file,
+                        err
+                    });
+                }
+                const target = this.getTargetTSFile(source, content)
+                resolve({
+                    file: source.file,
+                    json: content,
+                    ...target
+                })
+            })
+        }));
+        Promise.all(fileParsePromise)
+            .then(records => {
+                return records;
+            }).then(this.writingTargetFile.bind(this))
+            .catch(error => {
+                logger.error(error);
+            })
     }
 
+    getTargetTSFile(file, json) {
+        const outPut = pathLib.resolve(this.root, this.options.output);
+        const targetFileName = pathLib.join(outPut, file.resolvePosition).replace(pathLib.extname(file.file), '.ts');
+        const targetFileSimpleName = pathLib.basename(targetFileName,".ts");
+
+        return {
+            targetFileName,
+            tsContent:this.getTargetTSFileContent(targetFileName,json)
+        }
+    }
+
+    writingTargetFile(records) {
+        const promiseCallers = records.map(record=>{
+
+            return new Promise((resolve,reject)=>{
+                fsLib.writeFile(record.targetFileName,record.tsContent,(err,result)=>{
+                    if(err){
+                        return reject(err);
+                    }
+                    resolve(result);
+                })
+            })
+        })
+        return Promise.all(promiseCallers)
+    }
+    getTargetTSFileContent(targetFileSrc,json){
+        const fileSimpleName = pathLib.basename(targetFileSrc,".ts");
+        let contentStr = `export default class ${fileSimpleName} {`;
+        console.log(contentStr)
+        contentStr += '\n}';
+        return contentStr;
+    }
     starting() {
     }
 }
